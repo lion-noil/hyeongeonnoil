6016450fb8bbe23ba0b7b3a7e05d236ee1bb07ac
Lion noiL <108247128+lion-noil@users.noreply.github.com>
2025-04-22 16:53:17 +0900

    add all

---
+++ src/constants/calendarMeta.js
@@ -0,0 +1,12 @@
+export const countryNameMap = {
+  countryList: ["KR", "CN", "JP", "US", "GB", "DE", "HK"],
+  country: {
+    KR: "한국",
+    CN: "중국",
+    JP: "일본",
+    US: "미국",
+    GB: "영국",
+    DE: "독일",
+    HK: "홍콩"
+  }
+};
